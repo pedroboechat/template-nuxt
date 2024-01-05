7cbdfcfbe8236eeda2b897a02f3f6603fdfd61d2
Pedro Boechat <pedro@boech.at>
2024-01-05 18:49:16 -0300

    feat: new vuetify import and bump packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+
 export default defineNuxtConfig({
-  modules: ["@invictus.codes/nuxt-vuetify", "@nuxtjs/eslint-module"],
-  vuetify: {
-    moduleOptions: {
-      treeshaking: true,
-      useIconCDN: true,
-      styles: true,
-      autoImport: true,
+  build: {
+    transpile: ["vuetify"],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
+    },
+    "@nuxtjs/eslint-module",
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
     },
   },
 });
